f1b436812b943abb24837850310c4476595ca252
MkhGT <133373787+MkhGT@users.noreply.github.com>
2025-07-21 12:48:54 +0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="text-xl md:text-3xl font-bold mb-6">About Us</h2>
         <p className="text-sm md:text-lg text-gray-600 leading-relaxed text-justify md:text-center">
           PT Barayasa Maizan Abadi is an Indonesian company that produces eco-friendly agricultural and handcrafted products. We offer traditional bamboo items such as woven trays (tampah), bread baskets (irig), and rice cooking baskets (bakul)—designed to blend everyday function with cultural heritage.
-        </br> In addition to crafts, we also provide natural agricultural goods like dried Eucheuma cottonii seaweed and premium rice husks, supporting sustainable practices in food, farming, and wellness. Our mission is to empower communities through products that reflect the values of tradition, nature, and quality.
+        <br/> In addition to crafts, we also provide natural agricultural goods like dried Eucheuma cottonii seaweed and premium rice husks, supporting sustainable practices in food, farming, and wellness. Our mission is to empower communities through products that reflect the values of tradition, nature, and quality.
         </p>
       </div>
     </section>
